e412ed2ae7feffd95d1ca10eaf810a476cedbe17
Damal Ranula <ranuladamal@gmail.com>
2024-02-29 21:04:34 +0530

    values add to details table

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,11 @@ function App() {
   const [temperature, setTemperature] = useState("null");
   const [condition, setCondition] = useState("");
   const [image, setImage] = useState("");
+  const[location,setLocation] = useState("");
+  const[humidity,setHumidity] = useState("");
+  const[windSpeed,setWindSpeed] = useState("");
+  const[region,setRegion] = useState("");
+  const[country,setCountry] = useState("");
 
   const handleSearch = async () => {
     try {
@@ -20,83 +25,96 @@ function App() {
       setTemperature(data.current.temp_c);
       setCondition(data.current.condition.text);
       setImage(data.current.condition.icon);
+      setLocation(data.location.tz_id);
+      setHumidity(data.current.humidity);
+      setWindSpeed(data.current.wind_kph);
+      setRegion(data.location.region);
+      setCountry(data.location.country);
     } catch (error) {
       console.log('Error fetching data', error);
     }
   }
   return (
     <div id='container'>
-    <div id="heder" >
-      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom" >
-        <div className="col-md-3 mb-2 mb-md-0">
-          <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
-            <h3 id='title'>Weather Forcast</h3>
-            <svg className="bi" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-          </a>
-        </div>
+      <div id="heder" >
+        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom" >
+          <div className="col-md-3 mb-2 mb-md-0">
+            <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
+              <h3 id='title'>Weather Forcast</h3>
+              <svg className="bi" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
+            </a>
+          </div>
 
-        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" id="navButton">
-          <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
-          <li><a href="#" className="nav-link px-2">Forcast</a></li>
-          <li><a href="#" className="nav-link px-2">News</a></li>
-          <li><a href="#" className="nav-link px-2">Map</a></li>
-          <li><a href="#" className="nav-link px-2">About</a></li>
-        </ul>
-      </header>
-      <div className="row justify-content-center">
-      <div className="col-5">
-        <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="form-control" id="inputCity" placeholder="Enter City Name" />
-      </div>
-      <div className="col-auto">
-        <button type="submit" className="btn btn-primary mb-3" onClick={handleSearch}>Search</button>
-      </div>
-    </div>
-    </div>
-    <div id="space"></div>
-    
-
-    <div id="subTitle">
-      <h2>Current Weather Condition</h2>
-    </div>
-    <div id='space2'></div>
-    <div className='container'>
-      <div id='detalis1'>
-        <p id='cityName'>{city}</p>
-        <div id="weatherImage">
-          <img src={image} className="card-img-top" alt="..." />
-        </div>
-        <div>
-          <p id='tempValue'>{temperature}°C</p>
-          <p id='conditionValue'>{condition}</p>
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" id="navButton">
+            <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
+            <li><a href="#" className="nav-link px-2">Forcast</a></li>
+            <li><a href="#" className="nav-link px-2">News</a></li>
+            <li><a href="#" className="nav-link px-2">Map</a></li>
+            <li><a href="#" className="nav-link px-2">About</a></li>
+          </ul>
+        </header>
+        <div className="row justify-content-center">
+          <div className="col-5">
+            <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="form-control" id="inputCity" placeholder="Enter City Name" />
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-primary mb-3" onClick={handleSearch}>Search</button>
+          </div>
         </div>
       </div>
-    </div>
-    <table className="table table-success table-striped">
-    <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-  </tbody>
-</table>
+      <div id="space"></div>
 
-  </div>
+
+      <div id="subTitle">
+        <h2>Current Weather Condition</h2>
+      </div>
+      <div id='space2'></div>
+      <div className='row'>
+        <div className='col-4'>
+          <div className='container-icon'>
+            <div id='detalis1'>
+              <p id='cityName'>{city}</p>
+              <div id="weatherImage">
+                <img src={image} className="card-img-top" alt="..." />
+              </div>
+              <div>
+                <p id='tempValue'>{temperature}°C</p>
+                <p id='conditionValue'>{condition}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id='table' className='col-8'>
+          <table className="table table-success table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Location</th>
+                <th scope="col">Temperature</th>
+                <th scope="col">Humidity</th>
+                <th scope="col">Condition</th>
+                <th scope="col">wind speed</th>
+                <th scope="col">Region</th>
+                <th scope="col">Country</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">{location}</th>
+                <td>{temperature}°C</td>
+                <td>{humidity}</td>
+                <td>{condition}</td>
+                <td>{windSpeed}</td>
+                <td>{region}</td>
+                <td>{country}</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
