36596d9ee7abcf9b31807cd25e7b168859abde0c
Damal Ranula <ranuladamal@gmail.com>
2024-02-29 23:07:41 +0530

    Add bootstrap footer

--- src/App.tsx
+++ src/App.tsx
@@ -120,6 +120,34 @@ function App() {
 
         </div>
       </div>
+      <div>
+        <div className="container-footer">
+          <footer className="py-5">
+            <div className="row">
+              <div className="col-md-5 offset-md-6 mb-3">
+                <form >
+                  <h5>Subscribe to our newsletter</h5>
+                  <p>Monthly digest of what's new and exciting from us.</p>
+                  <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+                    <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+                    <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+                      <button className="btn btn-primary" type="button">Subscribe</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+            <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+              <p>&copy; 2024 Company, Inc. All rights reserved.</p>
+              <ul className="list-unstyled d-flex">
+                <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use href="#twitter" /></svg></a></li>
+                <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use href="#instagram" /></svg></a></li>
+                <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use href="#facebook" /></svg></a></li>
+              </ul>
+            </div>
+          </footer>
+        </div>
+      </div>
     </div>
   )
 }
